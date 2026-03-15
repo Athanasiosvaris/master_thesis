4774db1d6806c654004158b6423a4691df9a7837
Athanasios Varis <thanosv22@gmail.com>
2026-03-15 17:52:13 +0100

    feat:New architecture of Forecasting.py and pulsarConsumer.py

--- model/Forecasting.py
+++ model/Forecasting.py
@@ -1,22 +1,29 @@
+import __main__
+from ast import parse
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import joblib
 import os
 import psycopg2
+import boto3
+from botocore.client import Config
+import argparse
 
 pd.options.mode.chained_assignment = None
 tf.random.set_seed(0)
-
-CSV_FILE = "sorted_data.csv"
-CSV_FILE_60_BATCHES = "60_batch_values.csv"
 
 
 def connect_to_database():
 
     try:
         conn = psycopg2.connect(
-            "dbname= 'postgres' user='postgres' password='postgres' port=5432 "
+            dbname= "postgres", 
+            user="postgres", 
+            password="postgres",
+            host="localhost",
+            port=5432 
         )
         if conn:
             print("Database connection established")
@@ -26,13 +33,13 @@ def connect_to_database():
         print(e)
 
 
-def insert_data_to_db(con, forecasted_values_df: pd.DataFrame, table_name: str) -> bool:
+def insert_data_to_db(con, forecasted_values_df: pd.DataFrame, database_table_name: str) :
     try:
         cur = con.cursor()
         for _, row in forecasted_values_df.iterrows():
             cur.execute(
                 f"""
-                     INSERT INTO {table_name}
+                     INSERT INTO {database_table_name}
                      (sensor_timestamp, sensor_id, sensor_energy_value_prediction)
                      VALUES (%s, %s, %s)
                     """,
@@ -44,98 +51,167 @@ def insert_data_to_db(con, forecasted_values_df: pd.DataFrame, table_name: str) 
             )
         con.commit()
         cur.close()
-        return True
     except psycopg2.OperationalError as e:
         print("Unable to insert data into database\n")
         print(e)
         return False
 
+def check_if_table_exists(conn, database_table_name: str ):
+    try:
+        cur = conn.cursor()
+        cur.execute(f"""SELECT to_regclass('public.{database_table_name}')""")
+        exists = cur.fetchone()[0]
+        if exists == database_table_name.lower():
+            cur.execute(f"""DROP TABLE {database_table_name};""")
+            conn.commit()
 
-conn = connect_to_database()
-print("Waiting for CSV...")
+        cur.execute(
+            f"""CREATE TABLE {database_table_name} (
+            id SERIAL PRIMARY KEY,
+            sensor_id Integer,
+            sensor_timestamp timestamp,
+            sensor_energy_value_prediction double precision);"""
+        )
+        conn.commit()
+    except psycopg2.OperationalError as e:
+        print(e)
 
-while True:
-    if not os.path.exists(CSV_FILE) or not os.path.exists(CSV_FILE_60_BATCHES):
-        continue
+def boto3_client():
+    # Create a boto3 client for s3
+    s3_client = boto3.client(
+        's3',
+        endpoint_url="http://localhost:9002",
+        aws_access_key_id='rustfsadmin',
+        aws_secret_access_key='rustfsadmin',
+        config=Config(signature_version='s3v4'))
+    return s3_client
 
-    if os.path.exists(CSV_FILE):
-        help = "CSV_FILE"
-        actual_values_df = pd.read_csv(CSV_FILE)
+def fetch_model_and_scaler_from_s3(s3_client,device_name:str, bucket_name: str, model_path: str, retrain: bool):
+    
+    #s3.download_file('amzn-s3-demo-bucket', 'OBJECT_NAME', 'FILE_NAME')
+    #OBJECT_NAME is the name of the file in the bucket, FILE_NAME is the name you want to save it as locally
+    #Naming in rustfs: bucket_name/deviceX/timestamp/deviceX.keras or bucket_name/deviceX/initial/deviceX.keras
+    if retrain:
+        name = device_name+"/initial/"+device_name+".keras" # =>deviceX/initial/deviceX.keras
+        scaler_name = device_name + "/" + "initial" + "/scaler.save"
     else:
-        help = "CSV_FILE_60_BATCHES"
-        actual_values_df = pd.read_csv(CSV_FILE_60_BATCHES)
+        name = device_name+"/"+model_path+"/"+device_name+".keras"
+        scaler_name = device_name + "/" + model_path + "/scaler.save"
+    while True:
+        try:
+            s3_client.download_file(bucket_name, name, f"./{device_name}.keras")
+            s3_client.download_file(bucket_name, scaler_name, f"./scaler.save")
+            #If this downlo    ad files I want to retry
+            model = tf.keras.models.load_model(f"./{device_name}.keras")
+            scaler = joblib.load(f"./scaler.save")
+            return model, scaler
+        except Exception as e:
+            print("Unable to download model and scaler from S3\n")
+            print("Retrying...\n")
 
-    print("CSV received")
 
-    forecasted_values_df = actual_values_df[["sensor_timestamp", "sensor_id"]]
-    forecasted_values_df["sensor_timestamp"] = pd.to_datetime(
-        forecasted_values_df["sensor_timestamp"]
-    )
-    print(forecasted_values_df.dtypes)
-    forecasted_values_df["sensor_timestamp"] = forecasted_values_df[
-        "sensor_timestamp"
-    ] + pd.Timedelta(minutes=1)
+def fetch_actual_values_from_db(conn,database_table_name:str):
+    cur = conn.cursor()
+    query = f"select sensor_id,sensor_energy_value,sensor_timestamp from {database_table_name} order by sensor_timestamp desc limit 60;"
+    cur.execute(query)  
+    rows  = cur.fetchall()
+    rows = rows[::-1]  #reverse the order of the list
+    return rows
 
-    model = tf.keras.models.load_model("lstm_initial_model_600.keras")
+def dataset_preparation(rows: list):
 
-    id_values = actual_values_df["sensor_id"].values
-    timestamp_values = actual_values_df["sensor_timestamp"].values
-    sensor_energy_value = actual_values_df["sensor_energy_value"].values
+    df = pd.DataFrame(rows, columns=['sensor_id', 'sensor_energy_value', 'sensor_timestamp'])
+    #values = df["sensor_energy_value"].values.astype("float32")
+    #dataset = values.reshape(-1, 1)
+    
+    return df
 
-    # Reshaping in NumPy refers to modifying the dimensions of an existing array without changing its data. The reshape() function is used for this purpose.
-    # It reorganizes the elements into a new shape, which is useful in machine learning, matrix operations and data preparation.
-    sensor_energy_value_reshaped = sensor_energy_value.reshape(-1, 1)
-    final_results = []
+def forecasting(model,scaler, conn,bucket_name: str,database_table_name: str):
+        actual_values_df = dataset_preparation(fetch_actual_values_from_db(conn, "actualvalues"))
+        print("Actual values fetched from database")
+        print(actual_values_df)
+  
+        forecasted_values_df = actual_values_df[["sensor_timestamp", "sensor_id"]]
+        forecasted_values_df["sensor_timestamp"] = pd.to_datetime(
+            forecasted_values_df["sensor_timestamp"]
+        )
+        print(forecasted_values_df.dtypes)
+        forecasted_values_df["sensor_timestamp"] = forecasted_values_df[
+            "sensor_timestamp"
+        ] + pd.Timedelta(minutes=1)
 
-    # Normalization
-    scaler = joblib.load("scaler.save")
-    sensor_energy_value_scaled = scaler.transform(sensor_energy_value_reshaped)
+        #id_values = actual_values_df["sensor_id"].values
+        #timestamp_values = actual_values_df["sensor_timestamp"].values
+        sensor_energy_value = actual_values_df["sensor_energy_value"].values
 
-    # generate the input and output sequences
-    n_lookback = 60  # length of input sequences (lookback period)
-    n_forecast = 60  # length of output sequences (forecast period)
+        # Reshaping in NumPy refers to modifying the dimensions of an existing array without changing its data. The reshape() function is used for this purpose.
+        # It reorganizes the elements into a new shape, which is useful in machine learning, matrix operations and data preparation.
+        sensor_energy_value_reshaped = sensor_energy_value.reshape(-1, 1)
+        #final_results = []
 
-    # Initial sliding window
-    buffer = sensor_energy_value_scaled[-n_lookback:, 0]
-    input_seq = buffer.reshape(1, n_lookback, 1)
+        # Normalization
+        scaler = joblib.load("scaler.save")
+        sensor_energy_value_scaled = scaler.transform(sensor_energy_value_reshaped)
 
-    # Recursive multi-step forecast
-    predictions_scaled = []
+        # generate the input and output sequences
+        n_lookback = 60  # length of input sequences (lookback period)
+        n_forecast = 60  # length of output sequences (forecast period)
 
-    for _ in range(n_forecast):
-        next_scaled = model.predict(input_seq, verbose=0)[0, 0]
-        predictions_scaled.append(next_scaled)
+        # Initial sliding window
+        buffer = sensor_energy_value_scaled[-n_lookback:, 0]
+        input_seq = buffer.reshape(1, n_lookback, 1)
 
-        # roll window
-        input_seq[:, :-1, 0] = input_seq[:, 1:, 0]
-        input_seq[:, -1, 0] = next_scaled
+        # Recursive multi-step forecast
+        predictions_scaled = []
 
-    # Inverse scale
-    predictions = scaler.inverse_transform(
-        np.array(predictions_scaled).reshape(-1, 1)
-    ).flatten()
+        for _ in range(n_forecast):
+            next_scaled = model.predict(input_seq, verbose=0)[0, 0]
+            predictions_scaled.append(next_scaled)
 
-    # Add the predictions into the ForecastedDF
-    forecasted_values_df = forecasted_values_df.assign(
-        sensor_energy_value_prediction=predictions
-    )
-    print(forecasted_values_df)
+            # roll window
+            input_seq[:, :-1, 0] = input_seq[:, 1:, 0]
+            input_seq[:, -1, 0] = next_scaled
 
-    # Send into db
+        # Inverse scale
+        predictions = scaler.inverse_transform(
+            np.array(predictions_scaled).reshape(-1, 1)
+        ).flatten()
 
-    if help == "CSV_FILE":
-        if insert_data_to_db(
-            conn,
-            forecasted_values_df,
-            "ForecastedValues",
-        ):
-            os.remove(CSV_FILE)
-            print("CSV_FILE deleted")
-    else:
-        if insert_data_to_db(
-            conn,
-            forecasted_values_df,
-            "ForecastedValuesBatched",
-        ):
-            os.remove(CSV_FILE_60_BATCHES)
-            print("CSV_FILE_60_BATCHES deleted")
+        # Add the predictions into the ForecastedDF
+        forecasted_values_df = forecasted_values_df.assign(
+            sensor_energy_value_prediction=predictions
+        )
+        print(forecasted_values_df)
+
+        # Send into db
+        if bucket_name == "missingtimestamps":
+            insert_data_to_db( conn,forecasted_values_df,database_table_name)
+        else:
+            #It will be the batched so data
+            insert_data_to_db(conn,forecasted_values_df,database_table_name)
+            
+
+
+#ToDo:
+# - Make it be called from pulsarConsumer.py -> Read actual values from the database, make the predictions and then insert the forecasted values into the database.
+# - Fetch model; and scaler from rustfs 
+
+# args (bucket name, model name in rustfs, database table name to insert parameters)
+
+
+if __name__ == "__main__":
+    
+        #Add arguments for bucket name, model name in rustfs and database table name to insert parameters   
+        parser = argparse.ArgumentParser(description="Forecasting script for sensor data")
+        parser.add_argument("--bucket_name", type=str, required=True,choices=["batch", "missingtimestamp"] ,help="Name of the S3 bucket to fetch the model and scaler from")
+        parser.add_argument("--device_name", type=str, required=True, help="Name of the device to filter the data for")
+        parser.add_argument("--model_path", type=str, required=False, help="Name of the model to fetch from S3 (without file extension)")
+        parser.add_argument("--retrain", type=bool, required=True, help="Whether am forecasting for retrained model or for initial model")
+        args = parser.parse_args()   
+        
+        database_table_name = args.device_name + "_forecastedvalues"
+        conn = connect_to_database()
+        check_if_table_exists(conn, database_table_name)
+        s3_client = boto3_client()
+        model, scaler = fetch_model_and_scaler_from_s3(s3_client, args.device_name, args.bucket_name, args.model_path, args.retrain)
+        forecasting(model, scaler, conn, args.bucket_name,database_table_name)
--- model/pulsarConsumer.py
+++ model/pulsarConsumer.py
@@ -1,13 +1,24 @@
+from ast import parse
+
+import subprocess
+from typing import Optional
 import pulsar
 from pulsar.schema import *
 from dataPreparationFunc import dataPreparation
 import pandas as pd
 import os
 import psycopg2
+import argparse
 
 
-OUTPUT_FILE = "sorted_data.csv"
-TEMP_FILE = "sorted_data.tmp"
+RETRAIN_TIMESTAMPS = {
+    "first_batch": ["00:00:00", "00:09:59"],
+    "second_batch": ["00:10:00", "00:19:59"],
+    "third_batch": ["00:20:00", "00:29:59"],
+    "fourth_batch": ["00:30:00", "00:39:59"],
+    "fifth_batch": ["00:40:00", "00:49:59"],
+    "sixth_batch": ["00:50:00", "00:59:59"],
+}
 
 
 class Sensor(Record):
@@ -22,23 +33,26 @@ def connect_to_database():
 
     try:
         conn = psycopg2.connect(
-            "dbname= 'postgres' user='postgres' password='postgres' port=5432 "
+            dbname= "postgres", 
+            user="postgres", 
+            password="postgres",
+            host="localhost",
+            port=5432 
         )
         if conn:
-            print("Database connection establiished")
+            print("Database connection established")
             return conn
     except psycopg2.OperationalError as e:
         print("Unable to connect to database:\n")
         print(e)
 
-
-def insert_data_to_db(con, Sorted_Dataframe: pd.DataFrame):
+def insert_data_to_db(con, Sorted_Dataframe: pd.DataFrame, table_name: str) -> bool:
 
     cur = con.cursor()
     for _, row in Sorted_Dataframe.iterrows():
         cur.execute(
-            """
-                INSERT INTO ActualValues
+            f"""
+                INSERT INTO {table_name}
                 (sensor_timestamp, sensor_id, sensor_energy_value)
                 VALUES (%s, %s, %s)
                 """,
@@ -46,58 +60,140 @@ def insert_data_to_db(con, Sorted_Dataframe: pd.DataFrame):
         )
     con.commit()
     cur.close()
+    return True
 
 
-client = pulsar.Client("pulsar://localhost:6650")
-consumer = client.subscribe(
-    topic="modelConsumeTopic",
-    subscription_name="pythonSubscription",
-    schema=AvroSchema(Sensor),
-)
-print("Python Consumer started")
-
-data = []
-SortedDf = pd.DataFrame()
-# conn = connect_to_database()
-
-
-while True:
+def check_if_table_exists(conn, table_name: str ):
     try:
-        msg = consumer.receive(5000)
+        cur = conn.cursor()
+        cur.execute(f"""SELECT to_regclass('public.{table_name}')""")
+        exists = cur.fetchone()[0]
+        if exists == table_name.lower():
+            cur.execute(f"""DROP TABLE {table_name};""")
+            conn.commit()
 
-    except pulsar.Timeout:
-        print("No new messages received yet.Trying again...")
-        if data:
-            print("Current data...")
-            SortedDf = dataPreparation(data)
-            print(SortedDf)
-            SortedDf.to_csv(TEMP_FILE, index=False)
-            os.replace(TEMP_FILE, OUTPUT_FILE)
-            # insert_data_to_db(conn, SortedDf)
-            data.clear()
-        continue
-
-    try:
-        ex = msg.value()
-        data.append(
-            {
-                "sensor_id": ex.sensor_id,
-                "sensor_energy_value": ex.sensor_energy_value,
-                "sensor_timestamp": ex.sensor_timestamp,
-            }
+        cur.execute(
+            f"""CREATE TABLE {table_name} (
+            id SERIAL PRIMARY KEY,
+            sensor_id Integer,
+            sensor_timestamp timestamp,
+            sensor_energy_value double precision);"""
         )
+        conn.commit()
+    except psycopg2.OperationalError as e:
+        print(e)
 
-        # Acknowledge successful processing of the message
-        consumer.acknowledge(msg)
-    except Exception:
-        # Message failed to be processed
-        print(
-            "Could acknoledge the message. Sending negative acknowledge and closing consumer and client."
-        )
-        consumer.negative_acknowledge(msg)
-        break
+def check_retrain(SorteDf: pd.DataFrame, device_name: str,bucket_name:str) -> list[str]:
+    # Implementation for triggering retraining
+    result = []
+    last_timestamp = str(SorteDf['sensor_timestamp'].iloc[-1])
+    
+    for value in RETRAIN_TIMESTAMPS.values():
+        if value[1] in last_timestamp:
+            print(f"Triggering retrain for window beetween {value[0]} and {value[1]}")
+            print("lasttimestamp is ", last_timestamp)
+            result = [value[0], value[1]]
+            #Here I should call the retrain.py script to retrain the model with the new data.
+            #retrain.py(device_name, bucket_name,database_table)
+            break
+    return result
 
-print("Closing consumer")
-consumer.close()
-print("Closing client")
-client.close()
+def prepare__retrain_result(result: list[str]):
+    #It will return 2025-12-08-00-00-00, 2025-12-08-00-09-59
+    if result:
+        date_prefix = str(result[1]).split(" ")[0] 
+        first_timestamp = str(date_prefix + " " + result[0]).replace(" ", "-").replace(":", "-")
+        last_timestamp = str(result[1]).replace(" ", "-").replace(":", "-")
+        return first_timestamp, last_timestamp
+    else:
+        return None, None
+
+def forecasting(device_name: str, bucket_name: str,retrain: bool ,model_path: Optional[str] = None):
+    script_path = os.path.join(os.path.dirname(__file__), "Forecasting.py")
+    cmd = [
+        "python3", script_path,
+        "--device_name", device_name,
+        "--bucket_name", bucket_name,
+        "--retrain", str(retrain),
+    ]
+    if model_path:
+        cmd += ["--model_path", model_path]
+
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Forecasting.py failed:\n{result.stderr}")
+    else:
+        print(f"Forecasting.py completed successfully:\n{result.stdout}")
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Pulsar Consumer for Sensor Data")
+    parser.add_argument("--topic", type=str, required=True, help="Name of the topic to subscribe to")
+    parser.add_argument("--device_name", type=str, required=True, help="Name of the device to filter the data for")
+    args = parser.parse_args()
+    topic = args.topic
+    device_name = args.device_name
+
+    #topic = modelConsumeTopic
+    client = pulsar.Client("pulsar://localhost:6650")
+    consumer = client.subscribe(
+        topic=topic,
+        subscription_name="pythonSubscription",
+        schema=AvroSchema(Sensor),
+    )
+    print("Python Consumer started")
+
+    data = []
+    SortedDf = pd.DataFrame()
+    conn = connect_to_database()
+    database_table_name = device_name + "_actualvalues"
+    check_if_table_exists(conn,database_table_name)
+
+
+    while True:
+        try:
+            msg = consumer.receive(5000)
+
+        except pulsar.Timeout:
+            print("No new messages received yet.Trying again...")
+            if data:
+                print("Current data...")
+                SortedDf = dataPreparation(data)
+                print(SortedDf)
+                #Check if we need to trigger retraining
+                #results = check_retrain(SortedDf, device_name, bucket_name)
+                #if results: ->Retrain is triggered
+                    #first_timestamp, last_timestamp = prepare__retrain_result(results)
+                    #Forecasting.py should be called with the new data and the new model to make the predictions and then insert the data to the database.
+                    #forecasting(model, scaler, conn, bucket_name, database_table_name)
+                #Here I should call Forecasting.py to make the predictions and then insert the data to the database.
+                #Forecasting.py should be called with the new data and the new model to make the predictions and then insert the data to the database.
+                insert_data_to_db(conn, SortedDf, device_name)
+                forecasting(device_name=device_name, bucket_name="missingtimestamp", retrain=False)
+                data.clear()
+            continue
+
+        try:
+            ex = msg.value()
+            data.append(
+                {
+                    "sensor_id": ex.sensor_id,
+                    "sensor_energy_value": ex.sensor_energy_value,
+                    "sensor_timestamp": ex.sensor_timestamp,
+                }
+            )
+
+            # Acknowledge successful processing of the message
+            consumer.acknowledge(msg)
+        except Exception:
+            # Message failed to be processed
+            print(
+                "Could acknoledge the message. Sending negative acknowledge and closing consumer and client."
+            )
+            consumer.negative_acknowledge(msg)
+            break
+
+    print("Closing consumer")
+    consumer.close()
+    print("Closing client")
+    client.close()
